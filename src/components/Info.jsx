0aa5491df64d8b99bf0d1cc79f77f652d81f5868
ganga0311 <86521494+ganga0311@users.noreply.github.com>
2021-06-26 18:28:36 +0530

    Initial commit

---
+++ src/components/Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javscript and React .js</h1>
+      <p>
+        React is an open-source font-end javascript library for building user
+        interfaces or UI componenets of the react.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
---
+++ src/components/Info1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info1() {
+  return (
+    <div className="note">
+      <h1>Python and cybersecurity</h1>
+      <p>
+        Python is a advantegeous programming language for cybersecurity because
+        it can perform many functions.{" "}
+      </p>
+    </div>
+  );
+}
+
+export default Info1;
